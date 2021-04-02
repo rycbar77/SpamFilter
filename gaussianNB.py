78a0cfe75cfb1cc14a2b27427596712d0bcb9723
rycbar <rycbar17th@gmail.com>
2021-04-02 11:25:52 +0800

    remove redundant lines

--- gaussianNB.py
+++ gaussianNB.py
@@ -4,16 +4,16 @@ from multinomialNB import MultinomialNB
 
 class GaussianNB(MultinomialNB):
     # calculate mean(mu) and standard deviation(sigma) of the given feature
-    def feature_prob(self, feature):
+    def feature_proba(self, feature):
         mu = np.mean(feature)
         sigma = np.std(feature)
         return mu, sigma
 
     # the probability density for the Gaussian distribution
-    def gaussian_prob(self, mu, sigma, x):
+    def gaussian_proba(self, mu, sigma, x):
         return (1.0 / (sigma * np.sqrt(2 * np.pi)) *
                 np.exp(- (x - mu) ** 2 / (2 * sigma ** 2)))
 
     # given mu and sigma , return Gaussian distribution probability for target_value
-    def get_xj_prob(self, mu_sigma, target_value):
-        return self.gaussian_prob(mu_sigma[0], mu_sigma[1], target_value)
+    def get_xj_proba(self, mu_sigma, target):
+        return self.gaussian_proba(mu_sigma[0], mu_sigma[1], target)
--- multinomialNB.py
+++ multinomialNB.py
@@ -12,9 +12,9 @@ class MultinomialNB(object):
         self.class_prior = []
         self.alpha = alpha
         self.classes = None
-        self.conditional_prob = None
+        self.conditional_proba = None
 
-    def feature_prob(self, feature):
+    def feature_proba(self, feature):
         values = np.unique(feature)
         total_num = float(len(feature))
         value_prob = defaultdict(lambda: self.alpha / (total_num + len(values) * self.alpha))
@@ -34,48 +34,45 @@ class MultinomialNB(object):
             self.class_prior.append((c_num + self.alpha) / (sample_num + class_num * self.alpha))
 
         # P( xj | y=ck )
-        self.conditional_prob = {}
+        self.conditional_proba = {}
         for c in self.classes:
-            self.conditional_prob[c] = {}
+            self.conditional_proba[c] = {}
             for i in range(len(X[0])):
                 feature = X[np.equal(y, c)][:, i]
                 # print(np.equal(y,c))
                 # print(X[np.equal(y, c)])
-                self.conditional_prob[c][i] = self.feature_prob(feature)
+                self.conditional_proba[c][i] = self.feature_proba(feature)
         return self
 
-    def get_xj_prob(self, values_prob, target_value):
-        return values_prob[target_value]
+    def get_xj_proba(self, values_proba, target):
+        return values_proba[target]
 
     def predict_single(self, x):
         label = -1
         max_prob = 0
 
-        # class_prior * conditional_prob
+        # 先验概率*条件概率
         for c_index in range(len(self.classes)):
             current_class_prior = self.class_prior[c_index]
-            conditional_prob = 1.0
-            feature_prob = self.conditional_prob[self.classes[c_index]]
+            conditional_proba = 1.0
+            feature_prob = self.conditional_proba[self.classes[c_index]]
             j = 0
             for feature_i in feature_prob.keys():
-                conditional_prob *= self.get_xj_prob(feature_prob[feature_i], x[j])
+                conditional_proba *= self.get_xj_proba(feature_prob[feature_i], x[j])
                 j += 1
 
-            if current_class_prior * conditional_prob > max_prob:
-                max_prob = current_class_prior * conditional_prob
+            if current_class_prior * conditional_proba > max_prob:
+                max_prob = current_class_prior * conditional_proba
                 label = self.classes[c_index]
 
         return label
 
     def predict(self, X):
-        if X.ndim == 1:
-            return self.predict_single(X)
-        else:
-            labels = []
-            for i in range(X.shape[0]):
-                label = self.predict_single(X[i])
-                labels.append(label)
-            return labels
+        labels = []
+        for i in range(X.shape[0]):
+            label = self.predict_single(X[i])
+            labels.append(label)
+        return labels
 
     def words2vec(self, vocabList, inputSet):
         return_vec = [0] * len(vocabList)
@@ -89,29 +86,14 @@ class MultinomialNB(object):
     def make_vec(self, ham_path='./email/ham', spam_path='./email/spam', train=False):
         cab_ham, vocab_ham = load_file(ham_path)
         cab_spam, vocab_spam = load_file(spam_path)
-        tmp = set([])
-        if train:
-            self.vocab = vocab_ham | vocab_spam
-        else:
-            tmp = (vocab_ham | vocab_spam) - self.vocab
-        vo = list(self.vocab) + list(tmp)
-        labels = []
-        X = []
-        for document in cab_ham:
-            labels.append(0)
-            X.append(self.words2vec(vo, document))
-
-        for document in cab_spam:
-            labels.append(1)
-            X.append(self.words2vec(vo, document))
-
-        X = np.array(X)
-        y = np.array(labels)
-        return X, y
+        return self.vectorize(cab_ham, cab_spam, vocab_ham, vocab_spam, train)
 
     def make_vec_from_list(self, ham_list, spam_list, train=False):
         cab_ham, vocab_ham = load_file_from_list(ham_list)
         cab_spam, vocab_spam = load_file_from_list(spam_list)
+        return self.vectorize(cab_ham, cab_spam, vocab_ham, vocab_spam, train)
+
+    def vectorize(self, cab_ham, cab_spam, vocab_ham, vocab_spam, train):
         tmp = set([])
         if train:
             self.vocab = vocab_ham | vocab_spam
